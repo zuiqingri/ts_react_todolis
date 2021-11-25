508cfac17d0eb37a23bec9b41134138e992f782e
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-11-25 23:03:52 +0800

    List Toggle

--- src/modules/todoList/component/index.tsx
+++ src/modules/todoList/component/index.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import "../todoList.css";
 
 type TodoType = {
   id: number;
@@ -8,10 +9,43 @@ type TodoType = {
 
 type TodoListType = TodoType[];
 
+const initialState = [
+  { id: 1, text: "node.js", done: true },
+  { id: 2, text: "npx create-react-app --template typescript", done: true },
+  { id: 3, text: "Making React Todolist", done: false },
+];
+
 const TodoList = () => {
+  const [todos, setTodos] = React.useState(initialState);
+
+  const onToggle = (id: number) => () => {
+    setTodos(
+      todos.map((todo) =>
+        todo.id === id ? { ...todo, done: !todo.done } : todo
+      )
+    );
+  };
+  const onRemove = (id: number) => () => {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  };
   return (
     <div>
-      <h2>Todo List</h2>
+      <h2>Todo List({todos.length})</h2>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo.id} className={"TodoItem"}>
+            <span
+              onClick={onToggle(todo.id)}
+              className={todo.done ? "done" : ""}
+            >
+              {todo.text}
+            </span>
+            <span onClick={onRemove(todo.id)} className="remove">
+              (X)
+            </span>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
