5ceb8cc0b0165d5b2e6ec91cc8044302332ef967
zuiqingri <72846494+zuiqingri@users.noreply.github.com>
2021-11-25 23:40:28 +0800

    add todo by using Hooks

--- src/modules/todoList/component/index.tsx
+++ src/modules/todoList/component/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { FormEvent } from "react";
 import "../todoList.css";
 
 type TodoType = {
@@ -9,14 +9,36 @@ type TodoType = {
 
 type TodoListType = TodoType[];
 
-const initialState = [
-  { id: 1, text: "node.js", done: true },
-  { id: 2, text: "npx create-react-app --template typescript", done: true },
-  { id: 3, text: "Making React Todolist", done: false },
-];
+const initialState: TodoListType = [];
 
 const TodoList = () => {
   const [todos, setTodos] = React.useState(initialState);
+  const inputEl = React.useRef<HTMLInputElement>(null);
+  React.useEffect(() => {
+    setTimeout(() => {
+      setTodos([
+        { id: 1, text: "node.js", done: true },
+        {
+          id: 2,
+          text: "npx create-react-app --template typescript",
+          done: true,
+        },
+        { id: 3, text: "Making React Todolist", done: false },
+      ]);
+    }, 2000);
+  }, []);
+
+  const onSubmit = (e: FormEvent) => {
+    e.preventDefault();
+    setTodos([
+      ...todos,
+      {
+        id: Math.max(...todos.map((todo) => todo.id)) + 1,
+        text: String(inputEl.current?.value),
+        done: false,
+      },
+    ]);
+  };
 
   const onToggle = (id: number) => () => {
     setTodos(
@@ -31,21 +53,29 @@ const TodoList = () => {
   return (
     <div>
       <h2>Todo List({todos.length})</h2>
-      <ul>
-        {todos.map((todo) => (
-          <li key={todo.id} className={"TodoItem"}>
-            <span
-              onClick={onToggle(todo.id)}
-              className={todo.done ? "done" : ""}
-            >
-              {todo.text}
-            </span>
-            <span onClick={onRemove(todo.id)} className="remove">
-              (X)
-            </span>
-          </li>
-        ))}
-      </ul>
+      <form onSubmit={onSubmit}>
+        <input placeholder="Input to do" ref={inputEl} type="text" />
+        <button type="submit">Resister</button>
+      </form>
+      {todos.length ? (
+        <ul>
+          {todos.map((todo) => (
+            <li key={todo.id} className={"TodoItem"}>
+              <span
+                onClick={onToggle(todo.id)}
+                className={todo.done ? "done" : ""}
+              >
+                {todo.text}
+              </span>
+              <span onClick={onRemove(todo.id)} className="remove">
+                (X)
+              </span>
+            </li>
+          ))}
+        </ul>
+      ) : (
+        <div>Loading...</div>
+      )}
     </div>
   );
 };
